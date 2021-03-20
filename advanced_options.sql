188480d0de14e0641ed7272ed7c406bbd68ad685
Michalina <michalina.grabias@gmail.com>
2021-03-20 16:11:58 +0100

    finalised project

--- advanced_options.sql
+++ advanced_options.sql
@@ -23,7 +23,6 @@ CALL UpdateBookshelf("U5", "B11", "read");
 -- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- --
 
 -- Alert every time a user adds a "read" book to add a review
-
 CREATE TRIGGER added_review
 AFTER INSERT
 ON ratings FOR EACH ROW
@@ -38,14 +37,11 @@ INSERT INTO ratings
 VALUES 
 	("U2", "B15", 5, NULL);
 
-DESCRIBE ratings;
-
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 -- 3. Create an event and demonstrate how it runs
 -- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- --
 
 -- Log in last user activity
-
 CREATE TABLE user_activity
 (user_id INT NOT NULL AUTO_INCREMENT, 
 last_update TIMESTAMP,
@@ -63,37 +59,21 @@ END//
 
 DELIMITER ;
 
-
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- 4. Create a view that uses at least 3-4 base tables
+-- 4. Create a view that uses at least 3-4 base tables 
+-- Prepare and demonstrate a query that uses the view to produce a logically arranged result set for analysis
 -- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- --
 
-
-
---- create a view to show book info, average rating & sth
--- show me books with rating > X
-
- --- add something more here
--- Show all users users arranged by how many books they've read and what is their role
-CREATE VIEW all_users_ranking
-AS
-SELECT u.user_id, u.user_first_name, u.user_last_name, u.user_age, a.user_role, r.books_read
-FROM users AS u
-INNER JOIN
-users_affiliation AS a
-ON u.user_id = a.user_id
-INNER JOIN
-(SELECT r.user_id, COUNT(r.book_id) AS books_read
-FROM reading_status AS r
-WHERE r.r_status = "read"
-GROUP BY user_id
-ORDER BY books_read DESC) AS r
-ON u.user_id = r.user_id;
-
+-- Show information for all users that are currently reading book X
+SELECT user_id, user_first_name, user_last_name, user_age, user_role, books_read
+FROM all_users_ranking
+WHERE user_id IN 
+	(SELECT user_id 
+	FROM reading_status AS r
+	WHERE r.book_id = "B5" AND r.r_status = "currently-reading");
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- 5. Prepare and demonstrate a query that uses the view to produce a logically arranged result set for analysis
--- 6. Prepare an example query with group by and having to demonstrate how to extract data from your DB for analysis
+-- 5. Prepare an example query with group by and having to demonstrate how to extract data from your DB for analysis
 -- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- -- -- -- ---- -- -- -- -- --
 
 -- Select all students that have read more than 1 book
@@ -106,10 +86,3 @@ HAVING books_read >= 1;
 SELECT a.user_role, AVG(books_read) AS average_books_read
 FROM all_users_ranking AS a
 GROUP BY user_role;
-
--- Show me all users arranged by how many books they read
-SELECT r.user_id, COUNT(r.book_id) AS books_read
-FROM reading_status AS r
-WHERE r.r_status = "read"
-GROUP BY user_id
-ORDER BY books_read DESC;
